5b7ba8bf48befd082faf34450c5cbbb6632e7479
Iñaki Abete <inakiabt@gmail.com>
2013-04-23 16:23:35 -0300

    npm Required package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,20 @@
 class titanium {
+  include nodejs
   package { 'titanium':
     ensure   => latest,
     provider => 'npm',
+    require => Package['npm']
   }
 
   package { 'alloy':
     ensure   => latest,
     provider => 'npm',
+    require => Package['npm']
   }
 
   package { 'tishadow':
     ensure   => latest,
     provider => 'npm',
+    require => Package['npm']
   }
 }
